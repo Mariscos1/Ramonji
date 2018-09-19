6591f47dee5c928a81f8d3743cc1ffb4379fc6c2
Mariscos1 <42446685+Mariscos1@users.noreply.github.com>
2018-09-19 11:14:31 -0500

    Update main.js

--- main.js
+++ main.js
@@ -11,5 +11,9 @@ const kanjiData = [
 document.getElementById("data").innerHTML = `
 <link rel="stylesheet" type="text/css" href="something.css" />
 <h1 class="data-title">Kanji (${kanjiData.length})results<h1>
+${kanjiData.map(function(symbol){
+  return symbol.kan
+}).join('')}
+<p class="footer">These $(kanjiData.length} kanji were added recently. Check back soon for updates. UwU</p>
 `
 
