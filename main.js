367afc0dd7b519a453d30ccca9a58ddc7d764942
Mariscos1 <42446685+Mariscos1@users.noreply.github.com>
2018-09-19 18:48:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -11,6 +11,90 @@ const kanjiData = [
       kata: "イチ, イツ",
       meaning: "one"
   },
+  {
+      kan:"国",
+      hira: "くに",
+     kata: "コク",
+     meaning: "country"
+  },
+  {
+      kan:"会",
+      hira: "あ.う, あ.わせる, あつ.まる",
+      kata: "カイ, エ",
+      meaning:"meeting, meet, party, association, interview, join"
+  },
+  {
+      kan:"人",
+      hira: "ひと, -り, -と",
+      kata: "ジン, ニン",
+     meaning: "person"
+  },
+  {
+      kan:"年",
+      hira: "とし",
+      kata: "ネン",
+      meaning:"year"
+  },
+   {
+      kan:"大",
+      hira: "おお-, おお.きい, -おお.いに",
+      kata: "ダイ, タイ",
+      meaning: "large, big"
+   },
+  {
+     kan:"十",
+     hira: "とお, と",
+     kata: "ジュウ, ジッ, ジュッ",
+     meaning: "ten"
+  },
+  {
+     kan:"二",
+     hira:   "ふた, ふた.つ, ふたた.び",
+     kata: "ニ, ジ",
+     meaning: "two"
+  },
+  {
+     kan:"本",
+     hira: "もと",
+     kata:  "ホン",
+     meaning: "book, present, main, true, real"
+  },
+  {
+     kan:"中",
+     hira: "なか, うち, あた.る",
+     kata:  "チュウ",
+     meaning: "in, inside, middle, mean, center"
+  },
+  {
+      kan:"長",
+     hira: "なが.い, おさ",
+     kata:  "チョウ",
+     meaning: "long, leader"
+  },
+    {
+     kan: "出",
+     hira: "で.る, -で, だ.す, -だ.す, い.でる, い.だす",
+     kata:  "シュツ, スイ",
+     meaning: "exit, leave"
+    },
+  {
+     kan: "三",
+     hira:  "み, み.つ, みっ.つ",
+     kata:  "サン, ゾウ",
+     meaning:  "three"
+  },
+  {
+     kan: "同",
+     hira:  "おな.じ",
+     kata:  "ドウ",
+     meaning:   "same, agree, equal"
+  },
+  {
+     kan: "時",
+     hira: "とき, -どき",
+     kata:  "ジ",
+     meaning:  "time, hour"
+  }
   ];
 
 function kanjiTemplate(symbol){
