9a8f21f4baa6e0a334d2e2514996a08c59faf6d4
Mariscos1 <42446685+Mariscos1@users.noreply.github.com>
2018-09-24 11:02:45 -0500

    Update main.js

--- main.js
+++ main.js
@@ -109,7 +109,15 @@ function kanjiTemplate(symbol){
 }
 document.getElementById("data").innerHTML = `
       <link rel="stylesheet" type="text/css" href="something.css" />
-<h1 class="data-title" style="background-color: #F74E51;margin:0;"">Kanji (${kanjiData.length})results<h1>
+<style>
+  body{
+  background-color: #FED9D9;
+}
+</style>
+<body>
+<h1 class="data-title" style="background-color: #F74E51;margin:0;border:0">Kanji 
+(${kanjiData.length})results<h1>
 ${kanjiData.map(kanjiTemplate).join('')}
 <p class="footer">These ${kanjiData.length} kanji were added recently. Check back soon for updates. UwU</p>
+</body>
 `
