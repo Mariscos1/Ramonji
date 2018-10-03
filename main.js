09cf50c60bb0f2a7e6aa87848092a84a72e0a681
Mariscos1 <42446685+Mariscos1@users.noreply.github.com>
2018-10-03 11:41:52 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var xmlhttp = new XMLHttpRequest();
 var url = "https://mariscos1.github.io/Ramonji/kanji.json";
-const kanjiData;
+var kanjiData;
 
 xmlhttp.onreadystatechange = function() {
 if (this.readyState == 4 && this.status == 200) {
