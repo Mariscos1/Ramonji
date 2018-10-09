12e29e28870f900c6a2ee0233dad9f1d1588a4e4
Mariscos1 <42446685+Mariscos1@users.noreply.github.com>
2018-10-09 10:14:52 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,107 @@
-var xmlhttp = new XMLHttpRequest();
-var url = "https://mariscos1.github.io/Ramonji/kanji.json";
-var kanjiData;
-
-xmlhttp.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 200) {
-    kanjiData = JSON.parse(this.responseText);
- //   kanjiData.map(kanjiTemplate).join('');
-    }
-};
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
+const kanjiData = [
+  {
+      kan: "日",
+      hira:"ひ, -び, -か",
+      kata: "ニチ, ジツ",
+      meaning: "day, sun, Japan, counter for days"
+  },
+  {
+      kan: "一",
+      hira: "ひと-, ひと",
+      kata: "イチ, イツ",
+      meaning: "one"
+  },
+  {
+      kan:"国",
+      hira: "くに",
+     kata: "コク",
+     meaning: "country"
+  },
+  {
+      kan:"会",
+      hira: "あ.う, あ.わせる, あつ.まる",
+      kata: "カイ, エ",
+      meaning:"meeting, meet, party, association, interview, join"
+  },
+  {
+      kan:"人",
+      hira: "ひと, -り, -と",
+      kata: "ジン, ニン",
+     meaning: "person"
+  },
+  {
+      kan:"年",
+      hira: "とし",
+      kata: "ネン",
+      meaning:"year"
+  },
+   {
+      kan:"大",
+      hira: "おお-, おお.きい, -おお.いに",
+      kata: "ダイ, タイ",
+      meaning: "large, big"
+   },
+  {
+     kan:"十",
+     hira: "とお, と",
+     kata: "ジュウ, ジッ, ジュッ",
+     meaning: "ten"
+  },
+  {
+     kan:"二",
+     hira:   "ふた, ふた.つ, ふたた.び",
+     kata: "ニ, ジ",
+     meaning: "two"
+  },
+  {
+     kan:"本",
+     hira: "もと",
+     kata:  "ホン",
+     meaning: "book, present, main, true, real"
+  },
+  {
+     kan:"中",
+     hira: "なか, うち, あた.る",
+     kata:  "チュウ",
+     meaning: "in, inside, middle, mean, center"
+  },
+  {
+      kan:"長",
+     hira: "なが.い, おさ",
+     kata:  "チョウ",
+     meaning: "long, leader"
+  },
+    {
+     kan: "出",
+     hira: "で.る, -で, だ.す, -だ.す, い.でる, い.だす",
+     kata:  "シュツ, スイ",
+     meaning: "exit, leave"
+    },
+  {
+     kan: "三",
+     hira:  "み, み.つ, みっ.つ",
+     kata:  "サン, ゾウ",
+     meaning:  "three"
+  },
+  {
+     kan: "同",
+     hira:  "おな.じ",
+     kata:  "ドウ",
+     meaning:   "same, agree, equal"
+  },
+  {
+     kan: "時",
+     hira: "とき, -どき",
+     kata:  "ジ",
+     meaning:  "time, hour"
+  },
+  {
+    kan: "友",
+    hira: "とも",
+    kata: "トモ",
+    meaning: "friend"
+  }
+  ];
 
 function kanjiTemplate(symbol){
   return `
@@ -21,14 +113,6 @@ function kanjiTemplate(symbol){
   </div>
   `
 }
-
-function setup() {
-  noCanvas();
-}
-function hello(){
-    Objects.keys(kanjiData).length;
-}
-
 document.getElementById("data").innerHTML = `
       <link rel="stylesheet" type="text/css" href="something.css" />
 <style>
@@ -37,8 +121,8 @@ document.getElementById("data").innerHTML = `
 }
 </style>
 <body>
-<h1 class="data-title" style="background-color: #F74E51;margin:0;border:0"><a href="index.html" title="Title">Kanji (${5}) results</a><h1>
-${kanjiData.map(kanjiTemplate)}
-<p class="footer">These kanjiData.length kanji were added recently. Check back soon for updates. UwU</p>
+<h1 class="data-title" style="background-color: #F74E51;margin:0;border:0"><a href="index.html" title="Title">Kanji (${kanjiData.length})results</a><h1>
+${kanjiData.map(kanjiTemplate).join('')}
+<p class="footer">These ${kanjiData.length} kanji were added recently. Check back soon for updates. UwU</p>
 </body>
-` 
+`
